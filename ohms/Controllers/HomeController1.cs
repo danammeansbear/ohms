6281841f9764bf1801b5b13504c484d77fa1e62a
danam <danam@DESKTOP-N853CNV>
2022-02-25 01:04:32 -0500

    Add project files.

---
+++ ohms/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ohms.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
