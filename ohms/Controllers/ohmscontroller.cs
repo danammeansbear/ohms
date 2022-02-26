e9095464a276b394ca49bf7255bd8ef980c2fa22
Adam Dabdoub <danammeansbear@gmail.com>
2022-02-25 23:53:42 -0500

    ..actually...established queries...are you reading these ?

--- ohms/Controllers/ohmscontroller.cs
+++ ohms/Controllers/ohmscontroller.cs
@@ -9,7 +9,8 @@ namespace ohmscontroller.Controllers
         {
             return View();
         }
-
+        //
+        // GET: HelloWorld/CalculateOhmValue
         public IActionResult CalculateOhmValue(string bandAColor,string bandBColor, string bandCColor, string bandDColor,int numTimes = 1)
         {
             ViewData["bandAMessage"] = "Current Color of band A is : " + bandAColor;
@@ -17,7 +18,7 @@ namespace ohmscontroller.Controllers
             ViewData["bandCMessage"] = "Current Color of band C is : " + bandCColor;
             ViewData["bandDMessage"] = "Current Color of band D is : " + bandDColor;
             ViewData["NumTimes"] = numTimes;
-
+            //https://localhost:7143/HelloWorld/CalculateOhmValue?bandAColor=red&bandBColor=blue&bandCColor=brown&bandDColor=green&numTimes=4
 
             return View();
         }
